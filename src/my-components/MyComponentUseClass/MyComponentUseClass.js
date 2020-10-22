ba1f5d6ceb81f879a0d8010bc7997912c6beb980
sobadrush <sobadrush@icloud.com>
2020-10-22 23:44:56 +0800

    導入json-server

--- src/my-components/MyComponentUseClass/MyComponentUseClass.js
+++ src/my-components/MyComponentUseClass/MyComponentUseClass.js
@@ -42,8 +42,8 @@ class MyComponentUseClass extends Component { // 繼承Component類別
 
         // this.setState({ myPercent: `${event.target.value}%` });
         this.setState({ myPercent: `${event.target.value}%` }, () => {
-            alert(`測試setState第2個參數, 當state被設定完之後，就會執行。我們可以利用這個參數來作想在state改變後的事情`);
-        }); 
+            console.log(`測試setState第2個參數, 當state被設定完之後，就會執行。我們可以利用這個參數來作想在state改變後的事情`);
+        });
     }
 
     // HTML部分
@@ -52,13 +52,13 @@ class MyComponentUseClass extends Component { // 繼承Component類別
             <div style={{ border: '3px solid white', margin: '1cm' }}>
                 <h2>MyComponentUseClass</h2>
                 <h3>使用ES6 class建立的component</h3>
-                <button type="button" onClick={this.changeEmpName}>click ({this.empName})</button>
+                <button type="button" onClick={this.changeEmpName}>click ({ this.empName })</button>
 
                 <br />
                 <p>Default range slider:</p>
 
 
-                輸入%數: <input type="text" style={{ width: '20%' }} onKeyUp={this.changePercent}></input>
+                輸入%數: <input type="text" style={{ width: '20%' }} defaultValue={ this.state.myPercent } onKeyUp={ this.changePercent }></input>
 
                 <br />
                 我是進度條
